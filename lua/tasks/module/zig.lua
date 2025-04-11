eba8c06eccbb075cf15148a78bffd2e2cb77e860
Nenad Mikša <nenad.miksa@microblink.com>
2025-04-12 00:36:30 +0200

    [WIP] file run/debug support for zig

--- lua/tasks/module/zig.lua
+++ lua/tasks/module/zig.lua
@@ -1,11 +1,18 @@
 local Path = require( 'plenary.path' )
 local utils = require( 'tasks.utils' )
 
-local function build( module_config, _ )
+local function build_project( module_config, _ )
      return {
         cmd = 'zig',
-        args = vim.list_extend({ 'build' }, utils.split_args(module_config.global_zig_args)),
-        ignore_stdout = true,
+        args = vim.list_extend({ 'build' }, utils.split_args(module_config.global_zig_args))
+      }
+end
+
+local function debug_project( module_config, _ )
+     return {
+        cmd = 'zig',
+        args = vim.list_extend({ 'build', 'run' }, utils.split_args(module_config.global_zig_args)),
+        dap_name = module_config.dap_name,
       }
 end
 
@@ -14,8 +21,33 @@ local function clean( _, _ )
      return {
         cmd = 'rm',
         args = { '-rf', 'zig-out' },
-        ignore_stdout = true,
       }
+end
+
+local function run_file( module_config, _ )
+    local currentSource = vim.fn.expand( '%' )
+     return {
+        cmd = 'zig',
+        args = vim.list_extend({ 'run', currentSource }, utils.split_args(module_config.global_zig_args)),
+      }
+end
+
+local function test( module_config, _ )
+    local currentSource = vim.fn.expand( '%' )
+     return {
+        cmd = 'zig',
+        args = vim.list_extend({ 'test', currentSource }, utils.split_args(module_config.global_zig_args)),
+      }
+end
+
+local function debug( module_config, _ )
+    local command = run_file( module_config, nil )
+    if not command then
+        return nil
+    end
+
+    command.dap_name = module_config.dap_name
+    return command
 end
 
 return {
@@ -25,7 +57,11 @@ return {
     },
     condition = function() return Path:new( 'zig.build' ):exists() end,
     tasks = {
-        build = build,
-        clean = clean
+        build = build_project,
+        debug = debug_project,
+        clean = clean,
+        run_file = run_file,
+        debug_file = { run_file, debug },
+        test_file = test,
     }
 }
