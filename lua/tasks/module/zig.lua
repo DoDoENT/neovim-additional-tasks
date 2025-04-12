9a24cbb02bfda93046d7e03e6ebd9a2c0235f16e
Nenad Mikša <nenad.miksa@microblink.com>
2025-04-12 19:44:55 +0200

    zig improvements
    - debug and run support for current file (main and all tests)
    - build type and build step selection

--- lua/tasks/module/zig.lua
+++ lua/tasks/module/zig.lua
@@ -1,18 +1,19 @@
 local Path = require( 'plenary.path' )
 local utils = require( 'tasks.utils' )
 
-local function build_project( module_config, _ )
-     return {
-        cmd = 'zig',
-        args = vim.list_extend({ 'build' }, utils.split_args(module_config.global_zig_args))
-      }
-end
+local build_type_map = {
+    Debug = 'off',
+    ReleaseSafe = 'safe',
+    ReleaseSmall = 'small',
+    ReleaseFast = 'fast',
+}
 
-local function debug_project( module_config, _ )
-     return {
+local function build( module_config, _ )
+    local build_target = module_config.build_step
+    local build_type = build_type_map[module_config.build_type]
+    return {
         cmd = 'zig',
-        args = vim.list_extend({ 'build', 'run' }, utils.split_args(module_config.global_zig_args)),
-        dap_name = module_config.dap_name,
+        args = { 'build', build_target, '--release=' .. build_type }
       }
 end
 
@@ -21,15 +22,42 @@ local function clean( _, _ )
      return {
         cmd = 'rm',
         args = { '-rf', 'zig-out' },
-      }
+     }
+end
+
+local function clean_cache( _, _ )
+    -- note: requires unix shell
+     return {
+        cmd = 'rm',
+        args = { '-rf', '.zig-cache' },
+     }
 end
 
 local function run_file( module_config, _ )
     local currentSource = vim.fn.expand( '%' )
      return {
         cmd = 'zig',
-        args = vim.list_extend({ 'run', currentSource }, utils.split_args(module_config.global_zig_args)),
+        args = { 'run', '-O' .. module_config.build_type, currentSource, '--' }
       }
+end
+
+local function build_test_file(module_config, _)
+    local currentSource = vim.fn.expand('%')
+    local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
+    return {
+        cmd = 'zig',
+        args = { 'test', currentSource, '-femit-bin=.zig-cache/test-' .. srcFilename, '--test-no-exec', '-O' .. module_config.build_type },
+    }
+end
+
+local function debug_test_file(module_config, _)
+    local currentSource = vim.fn.expand('%')
+    local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
+    return {
+        cmd = '.zig-cache/test-' .. srcFilename,
+        dap_name = module_config.dap_name
+    }
+
 end
 
 local function test( module_config, _ )
@@ -40,28 +68,49 @@ local function test( module_config, _ )
       }
 end
 
-local function debug( module_config, _ )
-    local command = run_file( module_config, nil )
-    if not command then
-        return nil
-    end
+local function build_file_as_exe( module_config, _ )
+    local currentSource = vim.fn.expand('%')
+    local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
+     return {
+        cmd = 'zig',
+        args = { 'build-exe', currentSource, '-femit-bin=.zig-cache/run-' .. srcFilename, '-O' .. module_config.build_type },
+      }
+end
 
-    command.dap_name = module_config.dap_name
-    return command
+local function debug_file( module_config, _ )
+    local currentSource = vim.fn.expand('%')
+    local srcFilename = vim.fn.fnamemodify(currentSource, ':t:r')
+     return {
+        cmd = '.zig-cache/run-' .. srcFilename,
+        dap_name = module_config.dap_name,
+      }
+end
+
+local function get_build_steps()
+    local allSteps = vim.fn.systemlist( 'zig build --list-steps' )
+    local stepNames = {}
+    for _, step in ipairs( allSteps ) do
+        local firstWord = string.gmatch( step, "%w+" )()
+        table.insert( stepNames, firstWord )
+    end
+    return stepNames
 end
 
 return {
     params = {
         'dap_name',
-        'global_zig_args'
+        build_type = { 'Debug', 'ReleaseSafe', 'ReleaseFast', 'ReleaseSmall' },
+        build_step = get_build_steps,
     },
     condition = function() return Path:new( 'zig.build' ):exists() end,
     tasks = {
-        build = build_project,
-        debug = debug_project,
+        build = build,
         clean = clean,
+        clean_cache = clean_cache,
+        clean_all = { clean, clean_cache },
         run_file = run_file,
-        debug_file = { run_file, debug },
+        debug_file = { build_file_as_exe, debug_file },
         test_file = test,
+        debug_test_file = { build_test_file, debug_test_file },
     }
 }
